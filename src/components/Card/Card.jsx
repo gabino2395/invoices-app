57c930b6e83742d70940b549ff8798f80a89302a
gabino mendez <gabino_mendez@hotmail.com.ar>
2023-06-08 12:05:49 -0300

    refactorizando

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -10,18 +10,14 @@ const Card = ({ description, title, price, image }) => {
           <p className=" texto long-name">{title}</p>
         </div>
       </div>
-      {/* <img class="w-full h-[120px]" src={image} alt="Sunset in the mountains" /> */}
-      <div class="px-6 py-4">
-        {/* <div class="font-bold text-xl mb-2">{title}</div> */}
-        {/* <p class="text-gray-700 text-base">{description}</p> */}
-      </div>
-      <div class="px-6 pt-4 pb-2"> 
+      <div class="px-6 py-4"></div>
+      <div class="px-6 pt-4 pb-2">
         <span class="inline-block  bg-green-400  rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
           ${price}
         </span>
         <span class="inline-block  rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-            Stock {price}
-          </span>
+          Stock {price}
+        </span>
       </div>
     </div>
   );
--- src/db/db.js
+++ src/db/db.js
@@ -1,14 +1,14 @@
 export const initialDb = [
   {
     id: 1,
-    name: "Factura de ejemplo",
+    name: "Factura 2",
     date: "1994-12-12",
     item: "MacBook Pro5",
     total: 100,
   },
   {
     id: 2,
-    name: "Factura de ejemplo2",
+    name: "Factura 1",
     date: "1994-12-12",
     item: "MacBook Pro",
     total: 100,
